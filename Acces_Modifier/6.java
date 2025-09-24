011a5a1e93dfdf102bfa8e994ea032858dbf557d
garvGupta2005 <garvg310@gmail.com>
2025-09-24 08:27:28 +0530

    Resolved merge conflicts

---
+++ Acces_Modifier/6.java
@@ -0,0 +1,51 @@
+class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: ₹" + balance);
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    private final double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolder, double balance, double interestRate) {
+        super(accountNumber, accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displaySavingsAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: ₹" + getBalance());
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+}
+
+// Remove the Main class from this file. Move it to a new file named Main.java.
